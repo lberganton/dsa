bb1a646b9ef530cc3c538eef91854ed67d747aff
lberganton <lucasbberganton@gmail.com>
2025-03-15 20:36:11 -0300

    Adiciona teste para LinkedList em C

--- stack/c/test.c
+++ stack/c/test.c
@@ -8,7 +8,6 @@ void consumer(int val) {
 int main(void) {
   printf("ArrayStack:\n");
   ArrayStack* as = arraystack_create(0);
-
   arraystack_push(as, 1);
   arraystack_push(as, 2);
   arraystack_push(as, 3);
@@ -16,13 +15,26 @@ int main(void) {
   arraystack_push(as, 5);
   arraystack_push(as, 6);
   arraystack_push(as, 7);
-
   arraystack_pop(as, NULL);
   arraystack_pop(as, NULL);
   arraystack_pop(as, NULL);
-
   arraystack_foreach(as, consumer);
-
   arraystack_free(as);
+
+  printf("LinkedStack:\n");
+  LinkedStack* ls = linkedstack_create();
+  linkedstack_push(ls, 1);
+  linkedstack_push(ls, 2);
+  linkedstack_push(ls, 3);
+  linkedstack_push(ls, 4);
+  linkedstack_push(ls, 5);
+  linkedstack_push(ls, 6);
+  linkedstack_push(ls, 7);
+  linkedstack_pop(ls, NULL);
+  linkedstack_pop(ls, NULL);
+  linkedstack_pop(ls, NULL);
+  linkedstack_foreach(ls, consumer);
+  linkedstack_free(ls);
+
   return 0;
 }
